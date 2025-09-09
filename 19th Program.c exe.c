3a63dc549ce6b5e706e2860cf6d97c2f23d59381
suparba03 <suparba03@gmail.com>
2025-09-09 20:50:58 +0530

    Add files via upload

---
+++ 19th Program.c exe.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number 0 or 1: ");
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf(" Result is Head ");
+    }
+    else
+    {
+        printf(" Result is Tail ");
+    }
+    return 0;
+}
