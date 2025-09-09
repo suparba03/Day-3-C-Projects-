fcdd581a0db32acd95df3cf9a0832fb685414786
suparba03 <suparba03@gmail.com>
2025-09-09 21:26:20 +0530

    Add files via upload

---
+++ 22th Program.c exe.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    printf("Enter a number: ");
+    scanf("%d",&b);
+    printf("Enter a number: ");
+    scanf("%d",&c);
+    if(a>b && a>c)
+    {
+        printf("%d is the maximum number.\n ",a);
+    }
+    else if(b>a && b>c)
+    {
+        printf("%d is the maximum number.\n ",b);
+    }
+    else
+    {
+      printf("%d is the maximum number.\n ",c);
+    }
+    return 0;
+}
