571d2e4f9c5198e20be47e80baf7969572b94a08
suparba03 <suparba03@gmail.com>
2025-09-09 20:06:04 +0530

    Add files via upload

---
+++ 16th Program .c exe.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter roll no: ");
+    scanf("%d",&a);
+    if(a==100)
+    {
+    printf(" Roll 100 is present ");
+    }
+    return 0;
+}
