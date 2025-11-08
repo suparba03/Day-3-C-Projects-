06eb881c14f872db59185c7502853184918086e3
suparba03 <suparba03@gmail.com>
2025-11-08 15:44:24 +0530

    Add files via upload

---
+++ 56th Program.c exe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibonacci(int n)
+{
+    if(n==0)
+        return 0;
+    else if(n==1)
+        return 1;
+    else
+        return fibonacci(n-1)+fibonacci(n-2);
+}
+int main()
+{
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+    printf("Fibonacci series from 0 to n ");
+    for(int i=0;i<n;i++)
+    {
+        printf("\t%d",fibonacci(i));
+    }
+    return 0;
+}
