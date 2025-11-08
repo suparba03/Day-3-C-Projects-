77e72094961d11c27ac5d7f135e0e0cf43523b72
suparba03 <suparba03@gmail.com>
2025-11-08 15:12:34 +0530

    Add files via upload

---
+++ 52th Program.c exe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    char ch='A';
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=4-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
