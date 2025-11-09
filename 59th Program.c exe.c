697dc27c31c0151f9b510a9bd8e16228228c9c6d
suparba03 <suparba03@gmail.com>
2025-11-09 20:48:15 +0530

    Add files via upload

---
+++ 59th Program.c exe.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10];
+    int n,i;
+    int even=0,odd=0;
+    printf("Enter number of elements ");
+    scanf("%d",&n);
+    printf("Enter %d numbers\n ",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+            even++;
+        else
+            odd++;
+    }
+    printf("Even numbers %d\n ",even);
+    printf("Odd numbers %d\n ",odd);
+    return 0;
+}
