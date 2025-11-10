0243310830e74560e16aa55f142e4c707d161044
suparba03 <suparba03@gmail.com>
2025-11-10 21:30:37 +0530

    Add files via upload

---
+++ 24th Program.c exe.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Size of int is %zu bytes\n",size of (int));
+    printf("Size of float is %zu bytes\n",size of (float));
+    printf("Size of double is %zu bytes\n",size of (double));
+    printf("Size of char is %zu bytes\n",size of (char));
+    return 0;
+}
