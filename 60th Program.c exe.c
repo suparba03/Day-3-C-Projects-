22bf6cf985745f3c829fe2e18db338ee3e56e608
suparba03 <suparba03@gmail.com>
2025-11-11 10:39:56 +0530

    Replace matrix sum program with dynamic input version

--- 60th Program.c exe.c
+++ 60th Program.c exe.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 int main()
 {
-    int a[2][3]={
-        {1,2,3},
-        {4,5,6}
-    };
-    int sum=0;
-    for(int i=0;i<2;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            sum=sum+a[i][j];
+    int a[10][10], b[10][10], sum[10][10];
+    int r, c, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
-    printf("Sum of matrix is %d ",sum);
+
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Sum of matrices
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nSum of the matrices:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
+
